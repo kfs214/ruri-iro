833a79c8b8c260db6cd60233fc96511d8c4ef3da
kazukiUshijima <info@kfs214.net>
2023-12-07 22:10:21 +0900

    feat: ImageGroup

---
+++ src/features/Form/ImageGroup/ImageGroup.tsx
@@ -0,0 +1,25 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+import { QuestionsGroupWrapper } from '@/features/Form/QuestionsGroupWrapper';
+
+import { SelectImage } from './SelectImage';
+
+export function ImageGroup() {
+  return (
+    <QuestionsGroupWrapper groupName="画像を設定">
+      <Box>
+        <Typography variant="h5">プロフィール画像</Typography>
+        <Box mt={1}>
+          <SelectImage buttonText="画像を選択" type="profile" />
+        </Box>
+      </Box>
+      <Box mt={2}>
+        <Typography variant="h5">カバー画像</Typography>
+        <Box mt={1}>
+          <SelectImage buttonText="画像を選択" type="cover" />
+        </Box>
+      </Box>
+    </QuestionsGroupWrapper>
+  );
+}
