52dab4d0fd17f01501df08aebd7c4aa6ad8119a5
kazukiUshijima <info@kfs214.net>
2024-01-23 21:23:57 +0900

    feat: 再マウント時のスクロール位置

--- src/features/Form/index.tsx
+++ src/features/Form/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
 
 import Box from '@mui/material/Box';
+
+import { useAppStore } from '@/store/useAppStore';
 
 import { ImageGroup } from './ImageGroup';
 import { NameInputGroup } from './NameInputGroup';
@@ -9,6 +11,24 @@ import { PersonalPerspectivesGroup } from './PersonalPerspectivesGroup';
 import { TagGroup } from './TagGroup';
 
 export function Form() {
+  const { formScrollPosition, setFormScrollPosition } = useAppStore();
+  const scrollY = useRef(0);
+
+  const updateScroll = () => {
+    scrollY.current = window.scrollY;
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, formScrollPosition);
+    window.addEventListener('scroll', updateScroll);
+
+    return () => {
+      setFormScrollPosition(scrollY.current);
+      window.removeEventListener('scroll', updateScroll);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Box display="grid" gap={3}>
       <NameInputGroup />
--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -1,21 +1,30 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
+// TODO Featureに閉じる状態を分離するか検討
+// TODO UI Stateでは？
+
 type AppState = {
   showPreview: boolean;
+  formScrollPosition: number;
   setShowPreview: (
     event: React.ChangeEvent<HTMLInputElement>,
     checked: boolean,
   ) => void;
+  setFormScrollPosition: (scrollY: number) => void;
 };
 
 export const useAppStore = create<AppState>()(
   devtools((set) => ({
     showPreview: false,
+    formScrollPosition: 0,
     setShowPreview: (e, checked) => {
       set({
         showPreview: checked,
       });
+    },
+    setFormScrollPosition: (scrollY) => {
+      set({ formScrollPosition: scrollY });
     },
   })),
 );
