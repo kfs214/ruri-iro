04dad451d1839e2ca421a78431c066a7ca2b7b26
kazukiUshijima <info@kfs214.net>
2024-02-23 11:16:31 +0900

    refactor: エラーモニタリングのプロパティ名を統一

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -23,15 +23,17 @@ export function AppProvider({ children }: AppProviderProps) {
 
     const originalConsoleError = window.console.error;
     function onUnhandledRejection({ reason }: { reason: unknown }) {
-      dataLayer.pushEvent('unhandledPromiseRejection', { reason });
+      dataLayer.pushEvent('unhandledPromiseRejection', {
+        errorMessage: reason,
+      });
     }
     function onError({ error }: { error: unknown }) {
-      dataLayer.pushEvent('uncaughtException', { error });
+      dataLayer.pushEvent('uncaughtException', { errorMessage: error });
     }
 
     window.console.error = (...error) => {
       originalConsoleError(...error);
-      dataLayer.pushEvent('consoleError', { error });
+      dataLayer.pushEvent('consoleError', { errorMessage: error });
     };
     window.addEventListener('unhandledrejection', onUnhandledRejection);
     window.addEventListener('error', onError);
