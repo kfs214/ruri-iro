90676621497abb59537db504495f2e55fbb860d3
kazukiUshijima <info@kfs214.net>
2024-03-02 14:18:34 +0900

    test: delete cypress

--- cypress/e2e/app.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('Navigation', () => {
-  it('should navigate to the about page', () => {
-    cy.visit('http://localhost:3000/');
-
-    cy.get('body > header > div > div').contains('Next.js App Router');
-  });
-});
