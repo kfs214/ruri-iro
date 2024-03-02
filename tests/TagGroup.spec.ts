04ac3dfd2058218207d00c0a98e4274cc9aff62f
kazukiUshijima <info@kfs214.net>
2024-03-03 08:43:36 +0900

    test: TagGroup - delete tag

--- tests/TagGroup.spec.ts
+++ tests/TagGroup.spec.ts
@@ -46,4 +46,51 @@ test.describe('TagGroup', () => {
       await expect(page.getByText('test-tag-blur')).toHaveCount(2);
     });
   });
+
+  test.describe('can delete tag', () => {
+    test('should delete only expected tags', async ({ page }) => {
+      const tagInput = await page.getByRole('textbox', {
+        name: 'tag-input',
+      });
+
+      await tagInput.fill('test-tag-1');
+      await tagInput.press('Enter');
+      await tagInput.fill('test-tag-2');
+      await tagInput.press('Enter');
+      await tagInput.fill('test-tag-3');
+      await tagInput.press('Enter');
+      await tagInput.fill('test-tag-4');
+      await tagInput.press('Enter');
+      await tagInput.fill('test-tag-5');
+      await tagInput.press('Enter');
+
+      await expect(page.getByText('test-tag-1')).toHaveCount(2);
+      await expect(page.getByText('test-tag-2')).toHaveCount(2);
+      await expect(page.getByText('test-tag-3')).toHaveCount(2);
+      await expect(page.getByText('test-tag-4')).toHaveCount(2);
+      await expect(page.getByText('test-tag-5')).toHaveCount(2);
+
+      await page
+        .getByRole('button', { name: '#test-tag-2' })
+        .getByTestId('CancelIcon')
+        .click();
+
+      await expect(page.getByText('test-tag-1')).toHaveCount(2);
+      await expect(page.getByText('test-tag-2')).not.toBeVisible();
+      await expect(page.getByText('test-tag-3')).toHaveCount(2);
+      await expect(page.getByText('test-tag-4')).toHaveCount(2);
+      await expect(page.getByText('test-tag-5')).toHaveCount(2);
+
+      await page
+        .getByRole('button', { name: '#test-tag-4' })
+        .getByTestId('CancelIcon')
+        .click();
+
+      await expect(page.getByText('test-tag-1')).toHaveCount(2);
+      await expect(page.getByText('test-tag-2')).not.toBeVisible();
+      await expect(page.getByText('test-tag-3')).toHaveCount(2);
+      await expect(page.getByText('test-tag-4')).not.toBeVisible();
+      await expect(page.getByText('test-tag-5')).toHaveCount(2);
+    });
+  });
 });
